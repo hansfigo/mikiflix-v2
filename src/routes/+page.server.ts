d8bc8561bb0508731e91e016f784cabfe0835de7
hansfigo <hambasahaya0303@gmail.com>
2023-10-19 00:31:15 +0700

    add timeout checkin

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,43 +7,55 @@ import { redis } from '$lib/server/Redis';
 export const load = (async ({ fetch }) => {
 
     const getRecentAnime = async (): Promise<ApiCallResult<RecentAnime>> => {
-        const cached = await redis.get('recent')
-
+        const cached = await redis.get('recent');
 
         if (cached) {
             console.log("CACHE HIT (recent)");
-
-            const data = JSON.parse(cached)
+            const data = JSON.parse(cached);
 
             console.log("EEEEE", data);
 
-            if(data.results){
-                return data
+            if (data.results) {
+                return data;
             }
         }
 
         console.log("CACHE MISS (recent)");
 
-        let data
+        let data;
 
+        // Membuat AbortController
+        const controller = new AbortController();
+        const { signal } = controller;
+
+        // Set timeout
+        const timeout = 10000; // 10 detik
+
+        // Fetch data with timeout
         try {
-            const res = await fetch('/api/recent');
+            const res = await fetch('/api/recent', { signal });
             data = await res.json();
-        } catch (error) {
+        } catch (error: any) {
             console.log(error);
 
-            const res = await fetch('https://api.consumet.org/meta/anilist/recent-episodes');
-            data = await res.json();
-            console.log("RECENT", data);
+            // Jika waktu habis, ganti ke URL lain
+            if (error.name === 'AbortError') {
+                console.log("Timeout tercapai. Mengganti ke URL lain...");
+                const res = await fetch('https://api.consumet.org/meta/anilist/recent-episodes');
+                data = await res.json();
+                console.log("RECENT", data);
+            } else {
+                // Tangani kesalahan lainnya
+                console.error('Terjadi kesalahan:', error);
+            }
         }
 
         // Store the data in Redis with a TTL of 30 seconds
         redis.set('recent', JSON.stringify(data), 'EX', 1800);
 
-
-
         return data;
-    }
+    };
+
 
     // const getPopularAnime = async (): Promise<ApiCallResult<PopularAnime>> => {
     //     const cached = await redis.get('popular');
@@ -77,7 +89,7 @@ export const load = (async ({ fetch }) => {
 
             console.log("EEEEE", data);
 
-            if(data.results){
+            if (data.results) {
                 return data
             }
         }
