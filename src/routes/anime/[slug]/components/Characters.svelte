68f61d997110a4be5b8024c545dd271dbaefb2b0
nazladiohevin <nazlahevinbusiness@gmail.com>
2023-11-16 12:40:22 +0700

    Characters components : add role and language

--- src/routes/anime/[slug]/components/Characters.svelte
+++ src/routes/anime/[slug]/components/Characters.svelte
@@ -10,13 +10,19 @@
 	<div class="flex flex-wrap gap-6 justify-start items-start pt-8">
 		{#if anim.characters}
 			{#each anim.characters as chara}
-				<div class="flex gap-8 max-h-40 h-40 border-2 border-white overflow-hidden">
+				<div class="flex gap-x-3 max-h-40 h-40 border-2 border-white overflow-hidden">
 					<div class="max-w-[40%] h-full flex">
 						<img class="object-cover" src={chara.image} alt={chara.name.first} />
 					</div>
-					<p>{chara.name.full}</p>
+					<div class="flex flex-col mr-8 justify-between items-start">
+						<p>{chara.name.full}</p>
+						<p>{chara.role}</p>
+					</div>
 					{#if chara.voiceActors && chara.voiceActors[0] && chara.voiceActors[0].name}
-						<p>{chara.voiceActors[0].name.full}</p>
+						<div class="flex flex-col justify-between items-end">
+							<p>{chara.voiceActors[0].name.full}</p>
+							<p>{chara.voiceActors[0].language}</p>
+						</div>
 					{:else}
 						<p>No Data</p>
 					{/if}
