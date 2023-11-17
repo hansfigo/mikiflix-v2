fab66b3764e5c1a87308d48b39e011c82f35e771
nazladiohevin <nazlahevinbusiness@gmail.com>
2023-11-17 23:06:37 +0700

    Characters components : fix padding

--- src/routes/anime/[slug]/components/Characters.svelte
+++ src/routes/anime/[slug]/components/Characters.svelte
@@ -13,29 +13,33 @@
 
 <section>
 	<h1>Characters</h1>
-	<div class="flex flex-wrap gap-6 justify-between	 items-start pt-8 px-4">
+	<div class="flex flex-wrap gap-6 justify-between items-start pt-8 px-4">
 		{#if anim.characters}
 			{#each anim.characters as chara}
 				<div class="flex justify-between gap-x-3 p-5 w-[48%] max-h-48 h-48 border-[3px] border-white border-opacity-50 overflow-hidden rounded-xl">
-					<div class="max-w-[40%] h-full flex rounded-xl overflow-hidden">
-						<img class="object-cover" src={chara.image} alt={chara.name.first} />
-					</div>
-					<div class="flex flex-col mr-8 justify-between items-start">
-						<p>{chara.name.full}</p>
-						<p class="opacity-50">{capitalizeFirstLetter(chara.role)}</p>
-					</div>
-					{#if chara.voiceActors && chara.voiceActors[0] && chara.voiceActors[0].name}
-						<div class="flex flex-col justify-between items-end">
-							<p>{chara.voiceActors[0].name.full}</p>
-							<p class="opacity-50">{chara.voiceActors[0].language}</p>
+					<div class="w-[48%] flex gap-x-3">
+						<div class="h-full w-2/5 flex rounded-xl overflow-hidden">
+							<img class="w-full object-cover" src={chara.image} alt={chara.name.first} />
 						</div>
-					{:else}
-						<p>No Data</p>
-					{/if}
-					<div class="max-w-[40%] h-full flex rounded-xl overflow-hidden">
+						<div class="w-1/2 flex flex-col justify-between items-start text-left">
+							<p>{chara.name.full}</p>
+							<p class="opacity-50">{capitalizeFirstLetter(chara.role)}</p>
+						</div>
+					</div>
+					<div class="w-[48%] flex gap-x-3">										
 						{#if chara.voiceActors && chara.voiceActors[0] && chara.voiceActors[0].name}
-							<img class="object-cover" src={chara.voiceActors[0].image} alt={chara.voiceActors[0].image} />
+							<div class="w-1/2 flex flex-col justify-between items-end text-right">
+								<p>{chara.voiceActors[0].name.full}</p>
+								<p class="opacity-50">{chara.voiceActors[0].language}</p>
+							</div>
+						{:else}
+							<p>No Data</p>
 						{/if}
+						<div class="w-2/5 h-full flex rounded-xl overflow-hidden">
+							{#if chara.voiceActors && chara.voiceActors[0] && chara.voiceActors[0].name}
+								<img class="w-full object-cover" src={chara.voiceActors[0].image} alt={chara.voiceActors[0].image} />
+							{/if}
+						</div>
 					</div>
 				</div>
 			{/each}
