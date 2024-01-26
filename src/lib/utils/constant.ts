c98541dd92feb9c8ee0c31631d32ac619ddf489d
HansFigo <hambasahaya0303@gmail.com>
2024-01-26 11:41:06 +0700

    chore(constant.ts): add JIKAN_API_URL constant to store the base URL for Jikan API feat(jikan/anime/airing/server.ts): fetch data from Jikan API using JIKAN_API_URL and return it as JSON response

--- src/lib/utils/constant.ts
+++ src/lib/utils/constant.ts
@@ -1,6 +1,8 @@
 export let TMDB_URI = "https://api.themoviedb.org/3";
 export let GOGO_URI = "https://consumet-api-production-e852.up.railway.app/anime/gogoanime/"
 
+export let JIKAN_API_URL = "https://api.jikan.moe/v4/"
+
 export let JIKAN_URL = '/api/jikan/'
 export let ANILIST_URL = '/api/anilist/'
 export let GOGO_URL = '/api/gogo/'
--- src/routes/api/jikan/anime/airing/+server.ts
+++ src/routes/api/jikan/anime/airing/+server.ts
@@ -1,5 +1,12 @@
+import { JIKAN_API_URL } from '$lib/utils/constant';
+import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
-    return new Response();
+
+    const res = await fetch(JIKAN_API_URL + "top/anime?filter=airing&limit=10")
+
+    const data = await res.json()
+
+    return json(data);
 };
