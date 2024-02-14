c09f4b4aa738d5dbba1312a9b2ffa036ccdf9321
hansfigo <hambasahaya0303@gmail.com>
2024-02-14 13:44:15 +0700

    Added Watch Route

---
+++ src/routes/watch/[id]/+page.server.ts
@@ -0,0 +1,9 @@
+import type { PageServerLoad } from "./$types";
+
+export const load = (async ({params}) => {
+
+    const {id} = params
+
+    // const
+    return {};
+}) satisfies PageServerLoad;
