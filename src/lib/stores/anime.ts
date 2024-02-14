9bdc21c0f74778e72e194453fc73ec4a55389934
hansfigo <hambasahaya0303@gmail.com>
2024-02-14 14:10:26 +0700

    chore(anime.ts): comment out unused animeProvider function fix(anime.ts): fix typo in fetch URL for getTrendingAnime and getDetailAnime functions fix(anime.ts): add error handling for fetch requests in getRecentAnime, getTrendingAnime, getDetailAnime, and getStreamingLinks functions chore(anime.ts): remove unused function declaration chore(layout.server.ts): comment out unused return statement in load function

--- src/lib/stores/anime.ts
+++ src/lib/stores/anime.ts
@@ -1,22 +1,15 @@
 import { CONSUMET_API_BASE_URL } from "$lib/utils/constant";
 import { error } from '@sveltejs/kit';
 
-function animeProvider() {
-    return
-}
+// function animeProvider() {
+//     return
+// }
 
 export const useAnime = () => {
 
     async function getRecentAnime() {
         try {
-            const res = await fetch(CONSUMET_API_BASE_URL + 'meta/anilist/recent-episodes');
-
-            if (!res.ok) {
-                throw error(500, {
-                    message: "Failed to Fetch Recent Anime Data, Please Try Again Later"
-                })
-            }
-
+            const res = await fetch(`${CONSUMET_API_BASE_URL}meta/anilist/recent-episodes`);
             const data = await res.json();
 
             return data;
@@ -29,16 +22,46 @@ export const useAnime = () => {
     }
 
     async function getTrendingAnime() {
+        try {
+            const res = await fetch(`${CONSUMET_API_BASE_URL}meta/anilist/popular`);
+            const data = await res.json();
 
+            return data;
+        } catch (err) {
+            console.error('Error fetching recent anime:', err);
+            throw error(500, {
+                message: "Error Fething Data on Server, Please Try Again Later"
+            })
+        }
     }
 
     async function getDetailAnime(id: number) {
+        try {
+            const res = await fetch(`${CONSUMET_API_BASE_URL}meta/anilist/recent-episodes`);
+            const data = await res.json();
 
+            return data;
+        } catch (err) {
+            console.error('Error fetching recent anime:', err);
+            throw error(500, {
+                message: "Error Fething Data on Server, Please Try Again Later"
+            })
+        }
     }
 
     async function getStreamingLinks(id: number) {
-    }
+        try {
+            const res = await fetch(`${CONSUMET_API_BASE_URL}meta/anilist/watch/${id}`);
+            const data = await res.json();
 
+            return data;
+        } catch (err) {
+            console.error('Error fetching recent anime:', err);
+            throw error(500, {
+                message: "Error Fething Data on Server, Please Try Again Later"
+            })
+        }
+    }
 
     return { getRecentAnime, getDetailAnime, getStreamingLinks, getTrendingAnime }
 }
