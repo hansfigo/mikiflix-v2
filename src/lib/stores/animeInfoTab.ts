409078d02a681fb2d29567b2da84b63a5a58d0f4
hansfigo <hambasahaya0303@gmail.com>
2024-02-15 01:37:31 +0700

    fix(animeInfoTab.ts): fix month name formatting in Start Date and End Date values to display full month name instead of month number feat(animeInfoTab.ts): add support for displaying additional anime details such as Airing, Status, Season, Episodes, Episode Duration, Start Date, End Date, Popularity, and Studio in the animeDetail store

--- src/lib/stores/animeInfoTab.ts
+++ src/lib/stores/animeInfoTab.ts
@@ -1,5 +1,23 @@
 import type { AnimeInfo } from '$lib/types/anime';
 import { derived, writable } from 'svelte/store';
+
+function getMonthName(month: number): string {
+	const months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+	return months[month - 1];
+}
 interface Tab {
 	text: 'overview' | 'characters' | 'episodes';
 	href: string;
@@ -14,20 +32,60 @@ export const isTabActiveStore = writable({
 export const animeDetailStore = writable<AnimeInfo>();
 
 export const animeDetail = derived(animeDetailStore, ($anim) => {
-	return [
-		{ title: 'Airing', value: 'Ep ' + $anim.nextAiringEpisode?.episode },
-		{ title: 'Status', value: $anim.status },
-		{ title: 'Season', value: $anim.season },
-		{ title: 'Episodes', value: $anim.totalEpisodes },
-		{ title: 'Episode Duration', value: $anim.duration + ' mins' },
-		{
+	const items = [];
+
+	// Airing
+	if ($anim.nextAiringEpisode?.episode) {
+		items.push({ title: 'Airing', value: 'Ep ' + $anim.nextAiringEpisode?.episode });
+	}
+
+	// Status
+	if ($anim.status) {
+		items.push({ title: 'Status', value: $anim.status });
+	}
+
+	// Season
+	if ($anim.season) {
+		items.push({ title: 'Season', value: $anim.season });
+	}
+
+	// Episodes
+	if ($anim.totalEpisodes) {
+		items.push({ title: 'Episodes', value: $anim.totalEpisodes });
+	}
+
+	// Episode Duration
+	if ($anim.duration) {
+		items.push({ title: 'Episode Duration', value: $anim.duration + ' mins' });
+	}
+
+	// Start Date
+	if ($anim.startDate) {
+		items.push({
 			title: 'Start Date',
-			value: `${$anim.startDate?.day} ${$anim.startDate?.month} ${$anim.startDate?.year}`
-		},
-		{ title: 'End Date', value: $anim.endDate?.month },
-		{ title: 'Popularity', value: $anim.popularity },
-		{ title: 'Studio', value: $anim.studios }
-	];
+			value: `${$anim.startDate?.day} ${getMonthName($anim.startDate?.month!)} ${$anim.startDate?.year}`,
+		});
+	}
+
+	// End Date
+	if ($anim.endDate?.day) {
+		items.push({
+			title: 'End Date',
+			value: `${$anim.endDate?.day} ${getMonthName($anim.endDate?.month!)} ${$anim.endDate?.year}`,
+		});
+	}
+
+	// Popularity
+	if ($anim.popularity) {
+		items.push({ title: 'Popularity', value: $anim.popularity });
+	}
+
+	// Studio
+	if ($anim.studios) {
+		items.push({ title: 'Studio', value: $anim.studios });
+	}
+
+	return items;
 });
 
 export const tabStore = derived(animeDetailStore, ($anim): Tab[] => {
